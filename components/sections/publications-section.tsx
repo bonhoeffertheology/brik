6b159a1aa945f0f478e8758f25188736ddf44a3a
bonhoeffertheology <jelsayou@gmail.com>
2026-05-27 15:32:54 +0900

    Update publications-section.tsx

--- components/sections/publications-section.tsx
+++ components/sections/publications-section.tsx
@@ -79,7 +79,7 @@ export function PublicationsSection() {
                 
                 {/* 안내 문구 */}
                 <div className={`absolute -bottom-10 left-0 w-full transition-opacity duration-500 delay-700 ease-in-out ${isCenter ? "opacity-100" : "opacity-0 pointer-events-none"}`}>
-                  <p className="text-stone-400 text-[10px] md:text-xs text-center font-sans whitespace-nowrap">
+                  <p className="text-stone-400 text-[12px] md:text-xs text-center font-sans whitespace-nowrap">
                     책을 클릭하시면 구입하실 수 있습니다
                   </p>
                 </div>
